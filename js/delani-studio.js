8b53802aeb8f087cb03fb678ab178661b80272a7
Lorna Kamau <kamaulorna@gmail.com>
2020-04-08 12:35:15 +0300

    format all the code

--- js/delani-studio.js
+++ js/delani-studio.js
@@ -1,33 +1,33 @@
-$(document).ready(function() {
-    $(".design").click(function() {
-        $(".design-icon").toggle();
-        $(".design-description").toggle();
-    });
-    $(".development").click(function() {
-        $(".development-icon").toggle();
-        $(".development-description").toggle();
-    });
-    $(".product").click(function() {
-        $(".product-icon").toggle();
-        $(".product-description").toggle();
-    });
+$(document).ready(function () { //allow for toggling between icon and description
+	$(".design").click(function () {
+		$(".design-icon").toggle();
+		$(".design-description").toggle();
+	});
+	$(".development").click(function () {
+		$(".development-icon").toggle();
+		$(".development-description").toggle();
+	});
+	$(".product").click(function () {
+		$(".product-icon").toggle();
+		$(".product-description").toggle();
+	});
 });
 
-$(document).ready(function() {
-    $(".view").hover(function() {
-      $(".name").show();
-    }, function(){
-    $(".name").hide();
-      });
-  });
+$(document).ready(function () { //hover effect for portfolio images
+	$(".view").hover(function () {
+		$(".name").show();
+	}, function () {
+		$(".name").hide();
+	});
+});
 
-  function sendMsg(){
-    var name = $("#mce-FNAME").val();
-    var email =$("#mce-EMAIL").val();
-    var message=$("#mce-MMERGE2").val();
-    if (name === "" || email === ""){
-      alert("Please fill in the form!")
-    } else{
-    alert("Thank you " + name +  "! Your message has been received")
-  };
-  };
+function sendMsg() {
+	var name = $("#mce-FNAME").val();
+	var email = $("#mce-EMAIL").val();
+	var message = $("#mce-MMERGE2").val();
+	if (name === "" || email === "") {
+		alert("Please fill in all fields!")
+	} else {
+		alert("Thank you " + name + "! Your message has been received. We shall get back to you shortly.")
+	};
+};
